a86eb7acf2f249c7634c3e2803000da7d0a68313
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-24 22:51:29 -0500

    added collisions between the cars and the turtle

--- car_manager.py
+++ car_manager.py
@@ -4,25 +4,29 @@ MOVE_INCREMENT = 10
 from turtle import Turtle
 import random
 
-class CarManager(Turtle):
+class CarManager():
     def __init__(self):
-        super().__init__()
-        self.new_x = -290
-        self.cars = []
-        self.shape("square")
-        self.setheading(0)
-        self.shapesize(stretch_len=2, stretch_wid = 1)
-        self.pu()
-        self.color(random.choice(COLORS))
-        self.new_y = random.randint(-300, 300)
-        self.goto(self.new_x, self.new_y)
+        self.all_cars = []
+        
+        
+        
+    def create_Car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")    
+            new_car.setheading(0)
+            new_car.shapesize(stretch_len=2, stretch_wid = 1)
+            new_car.pu()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
         
     
-    
-    def movement(self):
-        self.fd(STARTING_MOVE_DISTANCE)
+    def move_cars(self):
+        for car in self.all_cars:
+            car.bk(STARTING_MOVE_DISTANCE)
         
-        # self.fd(STARTING_MOVE_DISTANCE)
         
         
         
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,25 +11,24 @@ screen.tracer(0)
 
 
 
-cars = []
+carmanager = CarManager()
 tim = Player()
-traffic = CarManager()
 screen.listen()
 screen.onkey(tim.up,"Up")
 
-j = 0
+
 game_is_on = True
 while game_is_on:
-    j+=1
     time.sleep(0.1)
     screen.update()
-    if j == 6:
-        car = CarManager()
-        cars.append(car)
-        j = 0
-    for c in cars:
-        c.movement()
-    # tim.turtle_movement()
+    carmanager.create_Car()
+    carmanager.move_cars()
+    
+    for car in carmanager.all_cars:
+        if car.distance(tim) <=20:
+            game_is_on = False
+    
+    
     tim.reset()
 
 
